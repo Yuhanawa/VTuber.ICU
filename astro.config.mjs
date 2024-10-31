4e899762178af3bd24ccea2645e398d6a0acbc0c
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-10-31 21:39:29 +0800

    feat: first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,16 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel/static';
+
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  output: 'static',
+  adapter: vercel(),
 });
