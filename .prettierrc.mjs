3562f7e8d4dc1baf920ecce3fd57e4f72ad99fad
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-11-01 01:11:15 +0800

    chore: now prettier support svelte

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,12 +1,18 @@
 /** @type {import("prettier").Config} */
 export default {
-    plugins: ['prettier-plugin-astro'],
-    overrides: [
-      {
-        files: '*.astro',
-        options: {
-          parser: 'astro',
-        },
+  plugins: ["prettier-plugin-astro",'prettier-plugin-svelte'],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
       },
-    ],
-  };
+    },
+    {
+      files: '*.svelte',
+      options: {
+        parser: 'svelte'
+      }
+    }
+  ],
+};
