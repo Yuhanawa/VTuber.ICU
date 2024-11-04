fc27b1b8dfaff0e79a2267929423ec6ea3b15301
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-11-04 21:24:46 +0800

    style: format

--- scripts/GenerateVs.ts
+++ scripts/GenerateVs.ts
@@ -1,16 +1,17 @@
 import { getUnames, getUnamesAndMids } from "../src/lib/InfoUtils";
 import fs from "fs";
 
-
-
 const namesAndMids = getUnamesAndMids();
 
-const vs = fs.readdirSync("src/content/v/", { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
+const vs = fs
+  .readdirSync("src/content/v/", { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
 
-namesAndMids.filter(({ uname,mid }) => !vs.includes(uname)).forEach(({ uname,mid }) => {
-  const content = `---
+namesAndMids
+  .filter(({ uname, mid }) => !vs.includes(uname))
+  .forEach(({ uname, mid }) => {
+    const content = `---
 mid: ${mid}
 title: "${uname}"
 pubDate: "${new Date().toISOString()}"
@@ -26,7 +27,7 @@ profile:
 ## 此条目有待补充
 你可以在[这里](https://github.com/Yuhanawa/VTuber.ICU/edit/master/src/content/v/${uname}/index.md)编辑本文
 `;
-  fs.mkdirSync(`src/content/v/${uname}`);
-  fs.writeFileSync(`src/content/v/${uname}/index.md`, content);
-  console.info(`已添加${uname}`);
-})
+    fs.mkdirSync(`src/content/v/${uname}`);
+    fs.writeFileSync(`src/content/v/${uname}/index.md`, content);
+    console.info(`已添加${uname}`);
+  });
