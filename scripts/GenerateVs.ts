c234689efe35dc3784941aa73f859838a652769f
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-11-04 21:24:10 +0800

    feat: add scripts:GenerateVs

---
+++ scripts/GenerateVs.ts
@@ -0,0 +1,32 @@
+import { getUnames, getUnamesAndMids } from "../src/lib/InfoUtils";
+import fs from "fs";
+
+
+
+const namesAndMids = getUnamesAndMids();
+
+const vs = fs.readdirSync("src/content/v/", { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+
+namesAndMids.filter(({ uname,mid }) => !vs.includes(uname)).forEach(({ uname,mid }) => {
+  const content = `---
+mid: ${mid}
+title: "${uname}"
+pubDate: "${new Date().toISOString()}"
+updatedDate: "${new Date().toISOString()}"
+profile:
+  {
+    Nickname: ["${uname}"],
+  }
+---
+
+关注[${uname}](https://space.bilibili.com/${mid})谢谢喵~ 关注[${uname}](https://space.bilibili.com/${mid})谢谢喵~
+
+## 此条目有待补充
+你可以在[这里](https://github.com/Yuhanawa/VTuber.ICU/edit/master/src/content/v/${uname}/index.md)编辑本文
+`;
+  fs.mkdirSync(`src/content/v/${uname}`);
+  fs.writeFileSync(`src/content/v/${uname}/index.md`, content);
+  console.info(`已添加${uname}`);
+})
